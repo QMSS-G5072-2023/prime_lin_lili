ac1a45b2a37f1dcf46ed507a4cab19180626623e
Lililam <ll3707@columbia.edu>
2023-10-30 23:31:05 -0400

    Add is_prime function.

---
+++ src/prime_ll3707/prime_ll3707.py
@@ -0,0 +1,21 @@
+import math
+
+def is_prime(n):
+     """This function check if a number is prime.
+    Parameter:
+    - n (int): The integer input to check.
+    Return:
+    - bool: True if the input integer is a prime number, False otherwise.
+    Examples:
+    ```python
+    >>> is_prime(3)
+    True
+    >>> is_prime(15)
+    False
+    """
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
